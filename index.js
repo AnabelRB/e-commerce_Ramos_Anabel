bfc6916ce10054fdb13be01849fb0d1f51a40f67
Anabel <anabelRB@gmail.com>
2024-09-17 20:40:05 -0300

    JavaS

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+let h1 = document.querySelector("h1");
+h1.innerText ='Productos';
+
 let array = [];
+
+function cards (){
+
 for (let i = 1; i < 10; i++) {
     array.push(`
         <div class="col-md-4 col-sm-6 col-12">
         <div class="card" style="width: 18rem;">
-        <div class="envoltura">
             <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg">
-        </div>
-            <div class>
+            <div>
                 <h4>Card ${i}</h4>
                 <p>Esta es la card numero ${i}</p>
            </div>
@@ -14,6 +18,8 @@ for (let i = 1; i < 10; i++) {
         </div>
     `);
 
-    document.querySelector("section").innerHTML = array.join().replaceAll(",", "");
 }
 
+document.querySelector("section").innerHTML = array.join().replaceAll(",", "");
+}
+cards();
