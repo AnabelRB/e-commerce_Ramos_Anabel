5578c462324764b367a9e1516c4997a390e4a360
Anabel <anabelRB@gmail.com>
2024-09-16 22:46:19 -0300

    Javascript initial

---
+++ index.js
@@ -0,0 +1,19 @@
+let array = [];
+for (let i = 1; i < 10; i++) {
+    array.push(`
+        <div class="col-md-4 col-sm-6 col-12">
+        <div class="card" style="width: 18rem;">
+        <div class="envoltura">
+            <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg">
+        </div>
+            <div class>
+                <h4>Card ${i}</h4>
+                <p>Esta es la card numero ${i}</p>
+           </div>
+        </div>
+        </div>
+    `);
+
+    document.querySelector("section").innerHTML = array.join().replaceAll(",", "");
+}
+
